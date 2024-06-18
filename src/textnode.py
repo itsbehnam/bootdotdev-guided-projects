6f9b786b8ecf8647553e0a95689918c1cc339846
Behnam <behnam.h1@gmail.com>
2024-06-18 08:31:43 +0300

    added text_node_to_html_node

--- src/textnode.py
+++ src/textnode.py
@@ -1,3 +1,5 @@
+from htmlnode import LeafNode
+
 class TextNode:
     def __init__(self, text, text_type, url):
         self.text = text
@@ -9,4 +11,22 @@ class TextNode:
 
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+# TODO: Using Enum for types
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case "text":
+            return LeafNode(value=text_node.text)
+        case "bold":
+            return LeafNode(tag="b", value=text_node.text)
+        case "italic":
+            return LeafNode(tag="i", value=text_node.text) 
+        case "code":
+            return LeafNode(tag="code", value=text_node.text)
+        case "link":
+            return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
+        case "image":
+            return LeafNode(tag="img", props={"src": text_node.url})
+        case _:
+            raise Exception('wrong type')
         
