0b3547bef824b62f21676c9d6c2deb9b46b5cb58
Behnam <behnam.h1@gmail.com>
2024-06-22 20:09:47 +0300

    split text nodes implemented

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode
+from textnode import TextNode, split_nodes_delimiter
 
 
 class TestTextNode(unittest.TestCase):
@@ -13,6 +13,18 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", "bold", "https://google.com")
         self.assertNotEqual(node, node2)
 
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_node_split_delimiter(self):
+        text_type_text = "text"
+        text_type_code = "code"
+        node = TextNode("This is text with a `code block` word", text_type_text)
+        new_nodes = split_nodes_delimiter([node], "`", text_type_code)
+        correct_value = [
+            TextNode("This is text with a ", text_type_text),
+            TextNode("code block", text_type_code),
+            TextNode(" word", text_type_text),
+        ]
+        self.assertEqual(new_nodes, correct_value)
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,15 @@
 from htmlnode import LeafNode
+import re
+
+text_type_text="text"
+text_type_bold="bold"
+text_type_italic="italic"
+text_type_code="code"
+text_type_link="link"
+text_type_image="image"
 
 class TextNode:
-    def __init__(self, text, text_type, url):
+    def __init__(self, text=None, text_type=None, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -30,3 +38,20 @@ def text_node_to_html_node(text_node):
         case _:
             raise Exception('wrong type')
         
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    result = []
+    for node in old_nodes:
+        if not isinstance(node, TextNode):
+            result.append(node)
+            continue
+
+        if node.text.count(delimiter) % 2 != 0:
+            raise Exception('Markdown: unmatched delimiter')
+        parts = node.text.split(delimiter)
+        for index, value in enumerate(parts):
+            if index % 2 == 0:
+                result.append(TextNode(value, node.text_type))
+            else:
+                result.append(TextNode(value, text_type))
+
+    return result
